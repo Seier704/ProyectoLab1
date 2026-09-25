66a5b5535b1f90191e21640b78d68f50ce7660f5
ProdByMts <105318985+javocccc@users.noreply.github.com>
2026-09-25 10:11:03 -0300

    Selecciono mi botón con queryselector

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const botonModoOscuro = document.querySelector('#modo-oscuro');
 botonModoOscuro.addEventListener('click', () => {
   document.body.classList.toggle('modo-oscuro');
 });
+
+const botonJavier = document.querySelector('#javier-toggle');
