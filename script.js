fdda0d333cfbee6332e06adad7463f5964038200
ProdByMts <nathanxx980@gmail.com>
2026-09-25 10:18:33 -0300

    Selecciono el boton y texto de mi seccion con querySelector

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const botonModoOscuro = document.querySelector('#modo-oscuro');
 botonModoOscuro.addEventListener('click', () => {
   document.body.classList.toggle('modo-oscuro');
 });
+
+const botonDetalles = document.querySelector('#btn-detalles-tuNombre');
+const infoExtra = document.querySelector('#info-extra-tuNombre');
